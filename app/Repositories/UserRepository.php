7a905b41403504d1fd0d4fad2b61eac3d6993bc8
Bruno Henrique <brunohenrique242@gmail.com>
2024-03-20 11:56:39 -0300

    feat: list users with pagination

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -3,10 +3,23 @@
 namespace App\Repositories;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class UserRepository
 {
     public function __construct(private User $user)
     {
+    }
+
+    public function getAllUsers(string $filter = ''): LengthAwarePaginator
+    {
+        return $this->user->where(
+            function ($query) use ($filter) {
+                if ($filter !== '') {
+                    $query->where('name', 'LIKE', "%{$filter}%");
+                }
+            }
+        )->paginate();
     }
 }
