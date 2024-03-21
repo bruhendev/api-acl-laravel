68242b532c01a3b21e0efe9ac25e35b48f0bae17
Bruno Henrique <brunohenrique242@gmail.com>
2024-03-21 15:57:04 -0300

    feat: logout and get user authenticated

--- app/Http/Controllers/Api/Auth/AuthApiController.php
+++ app/Http/Controllers/Api/Auth/AuthApiController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Auth\AuthApiRequest;
+use App\Http\Resources\UserResource;
 use App\Repositories\UserRepository;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
@@ -18,19 +21,28 @@ class AuthApiController extends Controller
     public function auth(AuthApiRequest $request)
     {
         $user = $this->userRepository->findByEmail($request->email);
-     
-        if (! $user || ! Hash::check($request->password, $user->password)) {
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
                 'email' => ['The provided credentials are incorrect.'],
             ]);
         }
-     
+        $user->tokens()->delete();
         $token = $user->createToken($request->device_name)->plainTextToken;
 
         return response()->json(['token' => $token]);
     }
 
-   
-        
+    public function me()
+    {
+        $user = Auth::user();
+        return new UserResource($user);
+    }
 
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return \response()->json([], Response::HTTP_NO_CONTENT);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,14 +16,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/auth', [AuthApiController::class, 'auth'])->name('auth');
+Route::post('/me', [AuthApiController::class, 'me'])->name('auth.me')->middleware('auth:sanctum');
+Route::post('/logout', [AuthApiController::class, 'logout'])->name('auth.logout')->middleware('auth:sanctum');
+Route::post('/auth', [AuthApiController::class, 'auth'])->name('auth.login');
 
-Route::apiResource('/permissions', PermissionController::class);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('/permissions', PermissionController::class);
 
-Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
-Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+});
+
 
 Route::get('/', fn () => response()->json(['message' => 'ok']));
